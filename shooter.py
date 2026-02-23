316f022b44a6a0f2924998a42d1bf0bf4f50ad89
DanielR723 <danielr723@stemia.ca>
2026-02-22 20:35:27 -0700

    Added some semi-accurate data, should be good for initial tests

--- shooter.py
+++ shooter.py
@@ -18,8 +18,8 @@ angle_range = np.pi / 4 # Tolerance for vertical angle when hitting the target (
 t_max = 10.0 # Max time to integrate to (s)
 speed_density = 1 # Number of values used when simulating speeds
 dir_density = 1 # Number of values used when simulating directions
-rpm_density = 10 # Number of values used when simulating rpms
-angle_density = 10 # Number of values used when simulating angles
+rpm_density = 100 # Number of values used when simulating rpms
+angle_density = 100 # Number of values used when simulating angles
 
 # Shooter Constraints
 speed_min = 0.0 # Minimum bot speed (m/s)
@@ -35,8 +35,8 @@ angle_max = 56.0 * np.pi / 180.0 # Maximum shooter angle (rad)
 m = 0.215 # Mass of the ball (kg)
 R = 0.0750 # Radius of the ball (m)
 A = np.pi * R**2 # Cross-sectional area of the ball (m^2)
-C_d = 1 # Drag coefficient (constant)
-k = 1 # S -> C_l approximate ratio (constant)
+C_d = 0.5 # Drag coefficient (constant)
+k = 0.3 # S -> C_l approximate ratio (constant)
 
 # Global Constants
 g = 9.81 # Gravitational acceleration (m/s^2)
@@ -45,7 +45,7 @@ p = 1.14 # Air density (kg/m^3)
 # Generate set of values to 
 speed = np.linspace(speed_min, speed_max, speed_density) # Speeds the bot is traveling (m/s)
 dir = np.linspace(dir_min, dir_max, dir_density) # Directions the bot is traveling (rad)
-rpm = np.linspace(rpm_min, rpm_max, rpm_density) # RPM's (2pi*rad/min)
+rpm = np.linspace(rpm_min, rpm_max, rpm_density) # RPM's (rotations/min)
 angle = np.linspace(angle_min, angle_max, angle_density) # Angle's (rad)
 
 # -------------------- Variables --------------------
@@ -54,22 +54,22 @@ angle = np.linspace(angle_min, angle_max, angle_density) # Angle's (rad)
 
 # -------------------- Data --------------------
 
-speed_deg = 3 # Degree of speed polynomial
-spin_deg = 3 # Degree of spin polynomial
+speed_deg = 2 # Degree of speed polynomial
+spin_deg = 2 # Degree of spin polynomial
 
 speed_c = [] # Coefficients for speed polynomial (NEEDS TO BE INITIALIZED BEFORE DATATABLE)
 spin_c = [] # Coefficients for spin polynomial (NEEDS TO BE INITIALIZED BEFORE DATATABLE)
 
-test_rpm = np.array([2000, 3000, 4000, 5000, 6000]) # RPM's (2pi*rad/min)
-test_speed = np.array([10, 20, 25, 30, 40]) # Speed exiting shooter (m/s)
-test_spin = np.array([100, 140, 145, 300, 325]) # Spin exiting shooter (2pi*rad/s)
+test_rpm = np.array([2000, 3000, 4000, 5000, 6000]) # RPM's (rotations/min)
+test_speed = np.array([3.0, 4.5, 7.2, 9.0, 9.0]) # Speed exiting shooter (m/s)
+test_spin = np.array([-3.43, -4.29, -4.00, -5.00, -6.32]) # Spin exiting shooter (rotations/s)
 
 num_tests = 1 # Number of tests
-timestep = 0.2 # Timestep between snapshots (s)
-tests = np.array([[2000, 32 * np.pi / 180]]) # [Motor RPM (2pi*rad/s), Hood Angle (rad)]
-test_x = np.array([[0.0, 0.2, 0.3, 0.35, 0.375]]) # X positions each frame (m)
-test_y = -np.array([[0.0, 0.08, 0.18, 0.42, 0.78]]) # Y positions each frame (m)
-t = np.arange(0, test_y.shape[1]) * timestep
+timestep = 0.033333 # Timestep between snapshots (s)
+tests = np.array([[3000, 51 * np.pi / 180]]) # [Motor RPM (rotations/s), Hood Angle (rad)]
+test_x = np.array([[0.00000, 0.11136, 0.22727, 0.34545, 0.46364, 0.59318, 0.67045, 0.77727, 0.89318, 1.00000, 1.11818, 1.22045, 1.32727, 1.43409]]) # X positions each frame (m)
+test_y = np.array([[0.39000, 0.51727, 0.64455, 0.76273, 0.86500, 0.95818, 1.03318, 1.09227, 1.16500, 1.22409, 1.27182, 1.29682, 1.32182, 1.34455]]) # Y positions each frame (m)
+t = np.arange(0, test_x.shape[1]) * timestep
 
 # -------------------- Data --------------------
 
@@ -97,7 +97,8 @@ def ode(t, y): # ODE's for integrator
 
     # Forces
     drag = -1 / 2 * p * C_d * A * speed * v
-    magnus = 1 / 2 * p * C_l * A * speed**2 * np.cross(np.array([0, 1, 0]), v / speed)
+    # magnus = 1 / 2 * p * C_l * A * speed**2 * np.cross(np.array([0, 1, 0]), v / speed)
+    magnus = 1 / 2 * p * C_l * A * speed * np.array([v[2], 0.0, -v[0]])
     gravity = np.array([0, 0, -m * g])
 
     # Differentials
@@ -121,7 +122,8 @@ def ode_c(t, y, C_d, k): # ODE's for integrator
 
     # Forces
     drag = -1 / 2 * p * C_d * A * speed * v
-    magnus = 1 / 2 * p * C_l * A * speed**2 * np.cross(np.array([0, 1, 0]), v / speed)
+    # magnus = 1 / 2 * p * C_l * A * speed**2 * np.cross(np.array([0, 1, 0]), v / speed)
+    magnus = 1 / 2 * p * C_l * A * speed * np.array([v[2], 0.0, -v[0]])
     gravity = np.array([0, 0, -m * g])
 
     # Differentials
@@ -143,7 +145,7 @@ def simulate(s, d, r, a): # Get the landing x for certain initial conditions
 
     # Set up integrator
     y0 = np.array([0.0, 0.0, shooter_height, s * np.cos(d) + speed * np.cos(a), s * np.sin(d), speed * np.sin(a), spin])
-    sol = solve_ivp(ode, t_span=(0.0, t_max), y0=y0, events=hit_event)
+    sol = solve_ivp(ode, t_span=(0.0, t_max), y0=y0, events=hit_event, rtol=1e-8, atol=1e-10)
 
     # Get return value
     if sol.t_events[0].size > 0:
@@ -182,7 +184,7 @@ def error(x):
 
         # Set up integrator
         y0 = np.array([0.0, 0.0, shooter_height, speed * np.cos(a), 0.0, speed * np.sin(a), spin])
-        sol = solve_ivp(ode_c, args=(C_d, k), t_span=(0.0, t[-1]), y0=y0, t_eval=t)
+        sol = solve_ivp(ode_c, args=(C_d, k), t_span=(0.0, t[-1]), y0=y0, t_eval=t, rtol=1e-8, atol=1e-10)
 
         est_x = sol.y[0]
         est_y = sol.y[2]
@@ -218,10 +220,10 @@ def gen_lookup(): # Create the lookup table
 
     # Integrate
     datatable = []
-    for i in range(speed_density):
-        for j in range(dir_density):
-            for k in range(rpm_density):
-                for l in range(angle_density):
+    for i_1 in range(speed_density):
+        for i_2 in range(dir_density):
+            for i_3 in range(rpm_density):
+                for i_4 in range(angle_density):
                     # Show perentage
                     current_iter += 1
                     percent = int(current_iter / percent_ratio * 100)
@@ -231,10 +233,10 @@ def gen_lookup(): # Create the lookup table
                         percent_done = percent
 
                     # Populate table
-                    s = speed[i]
-                    d = dir[j]
-                    r = rpm[k]
-                    a = angle[l]
+                    s = speed[i_1]
+                    d = dir[i_2]
+                    r = rpm[i_3]
+                    a = angle[i_4]
 
                     shot_dist, shot_dir = simulate(s, d, r, a)
                     if shot_dist:
@@ -267,7 +269,7 @@ def sim_path(s, d, r, a): # Plot the path of a projectile with certain starting 
 
     # Setup integrator
     y0 = np.array([0.0, 0.0, shooter_height, s * np.cos(d) + speed * np.cos(a), s * np.sin(d), speed * np.sin(a), spin])
-    sol = solve_ivp(ode, t_span=(0, t_max), y0=y0, events=hit_event)
+    sol = solve_ivp(ode, t_span=(0, t_max), y0=y0, events=hit_event, rtol=1e-8, atol=1e-10)
 
     # Plot data
     fig = plt.figure()
@@ -283,7 +285,7 @@ def sim_path(s, d, r, a): # Plot the path of a projectile with certain starting 
 
 def plot_shot_data(): # Plot the collected speed and spin datapoints against the polynomial curves
     # Get data
-    x = np.linspace(rpm_min, rpm_max, rpm_density)
+    x = np.linspace(rpm_min, rpm_max, 100)
     y1, y2 = rpm_to_params(x)
 
     # Plot data
@@ -301,7 +303,7 @@ def plot_test_shots():
 
         # Setup integrator
         y0 = np.array([0.0, 0.0, shooter_height, speed * np.cos(a), 0.0, speed * np.sin(a), spin])
-        sol = solve_ivp(ode, t_span=(0, t_max), y0=y0, events=hit_event)
+        sol = solve_ivp(ode, t_span=(0.0, t[-1]), y0=y0, events=hit_event, rtol=1e-8, atol=1e-10, max_step=0.01)
 
         # Plot data
         plt.plot(sol.y[0, :], sol.y[2, :])
@@ -316,5 +318,5 @@ def plot_test_shots():
 
 
 fit_shot_data()
-optimize_consts()
+# optimize_consts()
 gen_lookup()
